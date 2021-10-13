7176f679107ff63bb681ce122b340c0e36ac1813
David G <himynamesdave@users.noreply.github.com>
2021-10-13 20:10:51 +0100

    Update const.js

--- source/scripts/const.js
+++ source/scripts/const.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SUB_DOMAIN: 'staging-01', // dev version staging-01. Prod version - app
-  API_URL: "https://staging-01.stixify.com/api/v1/",
-  APP_URL: "https://staging-01.stixify.com/",
+  SUB_DOMAIN: 'app', // dev version staging-01. Prod version - app
+  API_URL: "https://app.stixify.com/api/v1/",
+  APP_URL: "https://app.stixify.com/",
 
   APP_SWITCH: "stixify", // obstracts | stixify
 
